34fb53485f1f13368484b3ba216bc1f7b7a53e96
stefaninilucas <lucas_stefanini@yahoo.com.br>
2025-08-12 15:31:20 -0300

    update

--- dashboard/graficos.py
+++ dashboard/graficos.py
@@ -3,6 +3,10 @@ import plotly_express as px
 
 def grafico_barra_emp(df, titulo, eixo_x, eixo_y, cores):
     
+    theme_base = st.get_option("theme.base")
+    font_color = "white" if theme_base == "dark" else "black"
+    bg_color = "black" if theme_base == "dark" else "white"
+
     st.subheader(titulo)
 
     # Ordena por posição
@@ -28,7 +32,7 @@ def grafico_barra_emp(df, titulo, eixo_x, eixo_y, cores):
             y=row[eixo_y],
             text=str(int(row['Total'])),
             showarrow=False,
-            font=dict(size=16, color='black'),
+            font=dict(size=16, color=font_color),
             xanchor='left',
             yanchor='middle'
         )
@@ -38,17 +42,21 @@ def grafico_barra_emp(df, titulo, eixo_x, eixo_y, cores):
         xaxis_title="",
         yaxis_title="",
         barmode='stack',
-        plot_bgcolor="white",
-        paper_bgcolor="white",
+        plot_bgcolor=bg_color,
+        paper_bgcolor=bg_color,
         font=dict(size=14),
         margin=dict(r=100),
-        yaxis=dict(tickfont=dict(size=16, color="black")),
+        yaxis=dict(tickfont=dict(size=16, color=font_color)),
         legend=dict(
-        orientation="h",
-        yanchor="bottom",
-        y=1,
-        xanchor="right",
-        x=1,
+            orientation="h",
+            yanchor="bottom",
+            y=1,
+            xanchor="right",
+            x=1,
+            font=dict(
+                size=14,
+                color=font_color
+            )    
         )
     )
 
